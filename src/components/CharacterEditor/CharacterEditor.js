272bd307320fdaabfd13ca0d24d481c15553b6ed
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2023-04-24 09:30:16 +0100

    feat: Remove z-index

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -25,8 +25,9 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspective} />
+
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
-        <div className={styles.perspective} />
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
